730e62d1cf520987f31f7ec221bde300dd1611e7
BKKang1 <network159@naver.com>
2022-10-17 13:41:20 +0900

    수정기능 완료

--- src/components/shop-detail/PriceComparison.js
+++ src/components/shop-detail/PriceComparison.js
@@ -17,12 +17,12 @@ function PriceComparison({ price, retail, wholesale }) {
     console.log(wholesale);
     if (retail.price) {
       setRetailGap(Math.abs(retail.price - price));
-      setRetailGapPer(Math.floor(retail.price / price) * 100);
+      setRetailGapPer(Math.floor(retailGap / 100));
     }
 
     if (wholesale.price) {
       setwholeSaleGap(Math.abs(wholesale.price - price));
-      setwholeSaleGapPer(Math.floor(wholesale.price / price) * 100);
+      setwholeSaleGapPer(Math.floor(wholeSaleGap / 100));
     }
   });
   if (retail.price != null && wholesale.price != null) {
